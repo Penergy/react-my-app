02e6bf873a560d722b58fdddb05cb831ed3d9fc5
Eason Peng <pengyc@rc.inesa.com>
2018-01-17 17:38:07 +0800

    add components

---
+++ src/components/LoginForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UsernameInput from 'UsernameInput.js'
+import UserPasswordInput from 'UserPasswordInput.js'
+
+export default class LoginForm extends React.Component {
+    render() {
+        return (
+            <h1> this is a LoginForm </h1>
+        )
+    }
+}
+
